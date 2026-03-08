75c8ad5a844df284c77058e235ced13f9e548a46
Priyankshu Sheet <priyankshu.sheet123@gmail.com>
2026-03-08 23:17:31 +0000

    Add keyboard shortcuts panel

--- src/App.tsx
+++ src/App.tsx
@@ -128,6 +128,7 @@ const App = () => {
                 <BrowserRouter>
                   <ScrollToTop />
                   <PWAInstallBanner />
+                  <KeyboardShortcuts />
                   <AnimatedRoutes />
                 </BrowserRouter>
               )}
